575d834275d7694102c21babb2fcadb4b43749d1
Ihor <ihorrykun@gmail.com>
2022-09-10 22:35:33 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,28 +6,26 @@ const buttonStop = document.querySelector('button[data-stop]');
 console.log(buttonStop);
 
 let timerId = 0;
-
+//  !   Функція  старту
 buttonStart.addEventListener('click', () => {
   buttonStart.disabled = true;
     buttonStop.disabled = false; 
+    //  !   Створюємо інтервал при якому змінюються кольори нашого body
     timerId = setInterval(() => {
       document.body.style.backgroundColor = getRandomHexColor();
     },1000);
-
-
 });
 
-
-
-
+// !    Функція радномного кольору
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
+//  !   Функція стопу 
 buttonStop.addEventListener('click', () => {
     buttonStop.disabled = true;
     buttonStart.disabled = false;
+    // !    Скидаємо  інтервал
     clearInterval(timerId);
 });
 
