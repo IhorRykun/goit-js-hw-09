2b9a8b2f1b76cf7f25792ba29b9fe8b67432b0ff
Ihor <ihorrykun@gmail.com>
2022-09-10 21:08:24 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
 
+const buttonStart = document.querySelector('button[data-start]');
+console.log(buttonStart);
+const buttonStop = document.querySelector('button[data-stop]');
+console.log(buttonStop);
+
+
+buttonStart.addEventListener('click', buttonClickStart);
+
+function buttonClickStart() {
+document.body.style.backgroundColor = getRandomHexColor();
+
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
