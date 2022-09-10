fe0445e40c95a81e53b3d84e74ce08c5c803cc5b
IhorRykun <ihorrykun@gmail.com>
2022-09-10 18:08:45 +0000

    Deploying to gh-pages from @ IhorRykun/goit-js-hw-09@2b9a8b2f1b76cf7f25792ba29b9fe8b67432b0ff 🚀

---
+++ 01-color-switcher.21ab92e9.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("button[data-start]");console.log(o);const t=document.querySelector("button[data-stop]");console.log(t),o.addEventListener("click",(function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}));
+//# sourceMappingURL=01-color-switcher.21ab92e9.js.map
---
+++ 01-color-switcher.34106c88.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("button[data-start]");console.log(o);var t=document.querySelector("button[data-stop]");console.log(t),o.addEventListener("click",(function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}))}();
+//# sourceMappingURL=01-color-switcher.34106c88.js.map
