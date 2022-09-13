7133055130b22ce3295e8ad5acdbdfbf96d88b19
Ihor <ihorrykun@gmail.com>
2022-09-13 22:36:07 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,61 +1,63 @@
+// Описан в документации
 import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+// Библиотека уведомлений
 import Notiflix from 'notiflix';
 
-const buttonStart = document.querySelector('[data-start]');
-const inputDalay = document.querySelector('#datetime-picker');
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('data-hours');
-const minutes = document.querySelector('data-minutes');
-const seconds = document.querySelector('data-seconds');
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
 
 
-flatpickr(inputDalay, options);
+// const buttonStart = document.querySelector('[data-start]');
+// const inputDalay = document.querySelector('#datetime-picker');
+// const days = document.querySelector('[data-days]');
+// const hours = document.querySelector('data-hours');
+// const minutes = document.querySelector('data-minutes');
+// const seconds = document.querySelector('data-seconds');
 
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-
-
-// const timer = {
-//   start() {
-//     const startTime = Date.now();
-
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = currentTime - startTime;
-//       const timeComponents = convertMs(deltaTime);
-//       console.log(timeComponents);
-//     }, 1000);
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
 //   },
 // };
-// timer.start();
+
+// flatpickr(inputDalay, options);
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+const timer = {
+  start() {
+    const startTime = Date.now();
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const timeComponents = convertMs(deltaTime);
+      console.log(timeComponents);
+    }, 1000);
+  },
+};
+timer.start();
+
 
 
