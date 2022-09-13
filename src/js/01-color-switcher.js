c0e81e7cb71e9321f1846451b5162ae6a81327e8
Ihor <ihorrykun@gmail.com>
2022-09-13 21:05:16 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,32 +1,30 @@
-import { remove, throttle } from "lodash";
+import { remove, throttle } from 'lodash';
 
 const buttonStart = document.querySelector('button[data-start]');
 console.log(buttonStart);
 const buttonStop = document.querySelector('button[data-stop]');
 console.log(buttonStop);
 
-let timerId = null;
-//  !   Функція  старту
-buttonStart.addEventListener('click', () => {
-  buttonStart.disabled = true;
-    buttonStop.disabled = false; 
-    //  !   Створюємо інтервал при якому змінюються кольори нашого body
-    timerId = setInterval(() => {
-      document.body.style.backgroundColor = getRandomHexColor();
-    },1000);
-});
-
 // !    Функція радномного кольору
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-//  !   Функція стопу 
+//  !   Функція стопу
 buttonStop.addEventListener('click', () => {
-    buttonStop.disabled = true;
-    buttonStart.disabled = false;
-    // !    Скидаємо  інтервал
-    clearInterval(timerId);
+  buttonStop.disabled = true;
+  buttonStart.disabled = false;
+  // !    Скидаємо  інтервал
+  clearInterval(timerId);
 });
 
-
+let timerId = null;
+//  !   Функція  старту
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  //  !   Створюємо інтервал при якому змінюються кольори нашого body
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,39 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+// import Notiflix from 'notiflix';
+
+const timer = {
+    start() {
+        const startTime = Date.now();
+
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = currentTime - startTime;
+const timeComponents = convertMs(deltaTime);
+            console.log(timeComponents);
+        }, 1000);
+    }
+};
+timer.start();
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
